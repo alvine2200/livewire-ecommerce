0082b0dff99a6372818afa919135be13424ba186
alvine2200 <alvinellavu@gmail.com>
2022-12-17 11:59:03 +0300

    majestic admin template bootstrap

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    {{-- admin template css --}}
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('admin/vendors/mdi/css/materialdesignicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/vendors/base/vendor.bundle.base.css') }}">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <link rel="stylesheet" href="{{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('admin/css/style.css') }}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{ asset('public/admin/images/favicon.png') }}" />
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'public/assets/css/bootstrap.min.css', 'public/assets/js/bootstrap.bundle.min.js', 'public/assets/js/jquery-3.6.2.min.js'])
+    @livewireStyles()
+</head>
+
+<body>
+
+    <div class="container-scroller">
+        @include('layouts.includes.navbar')
+        <div class="container-fluid page-body-wrapper">
+            @include('layouts.includes.sidebar')
+
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('admin/vendors/base/vendor.bundle.base.js') }}"></script>
+    <script src="{{ asset('admin/vendors/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('admin/vendors/datatables.net/jquery.dataTables.js') }}">
+        < /scrip> <
+        script src = "{{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}" >
+    </script>
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="{{ asset('admin/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('admin/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('admin/js/template.js') }}"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <script src="{{ asset('admin/js/dashboard.js') }}"></script>
+    <script src="{{ asset('admin/js/data-table.js') }}"></script>
+    <script src="{{ asset('admin/js/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('admin/js/dataTables.bootstrap4.js') }}"></script>
+    <!-- End custom js for this page-->
+    @livewireScripts()
+</body>
+
+</html>
